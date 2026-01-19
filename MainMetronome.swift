5237b3b252afb93ce95bccfa21c6ffaa97c885f4
SHChalmers <simonhan@chalmers.se>
2026-01-19 19:13:40 +0100

    Refactor BarView and enhance RudimentList
    Refactor BarView layout and improve RudimentList functionality

--- MainMetronome.swift
+++ MainMetronome.swift
@@ -11,7 +11,7 @@ struct BarView: View {
     @State private var selectedTab = 0
     
     var body: some View {
-        VStack(spacing: 0) {
+        ZStack {
             TabView(selection: $selectedTab) {
                 MainMetronome()
                     .tag(0)
@@ -19,34 +19,39 @@ struct BarView: View {
                     .tag(1)
             }
             //.tabViewStyle(.page(indexDisplayMode: .never))
-            HStack() {
+            
+            VStack {
                 Spacer()
-                Button(action: {
-                    withAnimation {
-                        selectedTab = 0
+                HStack() {
+                    Spacer()
+                    Button(action: {
+                        withAnimation {
+                            selectedTab = 0
+                        }
+                    }) {
+                        Image(systemName: "metronome.fill")
+                            .font(.system(size:30))
+                            .foregroundColor(selectedTab == 0 ? .blue : .white)
                     }
-                }) {
-                    Image(systemName: "metronome.fill")
-                        .font(.system(size:30))
-                        .foregroundColor(selectedTab == 0 ? .blue : .white)
-                }
-                Spacer()
-                Button(action: {
-                    withAnimation {
-                        selectedTab = 1
+                    Spacer()
+                    Button(action: {
+                        withAnimation {
+                            selectedTab = 1
+                        }
+                    }) {
+                        Image(systemName: "music.note.list")
+                            .font(.system(size:30))
+                            .foregroundColor(selectedTab == 1 ? .blue : .white)
                     }
-                }) {
-                    Image(systemName: "music.note.list")
-                        .font(.system(size:30))
-                        .foregroundColor(selectedTab == 1 ? .blue : .white)
+                    Spacer()
                 }
-                Spacer()
+                .frame(maxWidth: .infinity)
+                .frame(height: 50)
+                .frame(alignment: .center)
+                .background(Color(red: 0.11, green: 0.11, blue: 0.12))
             }
-            .frame(maxWidth: .infinity)
-            .frame(height: 50)
-            .frame(alignment: .center)
-            .background(Color(red: 0.11, green: 0.11, blue: 0.12))
         }
+        .toolbarBackground(.hidden, for: .navigationBar)
     }
 }
 
@@ -156,7 +161,7 @@ struct MainMetronome: View {
             VStack {
                 Text("\(Int(tempo))")
                     .font(.system(size: 50))
-                    .foregroundColor(.white)
+                    .foregroundStyle(.white)
                 
                 Slider(value: $tempo, in: 30...300)
                     .onChange(of: tempo) {
@@ -247,29 +252,77 @@ struct Rudiment: Identifiable, Codable {
 
 struct RudimentList: View {
     @State private var rudiments: [Rudiment] = []
-    var body: some View {
-        ZStack {
-            Color(red: 0.11, green: 0.11, blue: 0.12)
-                .ignoresSafeArea()
-            List(rudiments) { rudiment in
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text(rudiment.name)
-                            .foregroundColor(.white)
-                        Text(rudiment.category)
-                            .font(.caption)
-                            .foregroundColor(.gray)
-                    }
-                }
-                .listRowBackground(Color(red: 0.11, green: 0.11, blue: 0.12))
-            }
-            .scrollContentBackground(.hidden)
-            .background(Color(red: 0.11, green: 0.11, blue: 0.12))
-        }
-        .onAppear {
-            loadRudiments()
+    @State private var showingMenu: Bool = false
+    @State private var selectedCategory: String = "Single Beat Combinations"
+    
+    var categories: [String] {
+        Array(Set(rudiments.map { $0.category })).sorted()
+    }
+    
+    var sortedRudiments: [Rudiment] {
+        if selectedCategory.isEmpty {
+            return rudiments
+        } else {
+            return rudiments.filter { $0.category == selectedCategory }
         }
     }
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(red: 0.11, green: 0.11, blue: 0.12)
+                    .ignoresSafeArea()
+                VStack {
+                    HStack {
+                        Spacer()
+                        Button("Category") {
+                            showingMenu = true
+                        }
+                        .padding(.trailing, 45)
+                        .confirmationDialog("Select Category", isPresented: $showingMenu) {
+                            ForEach(categories, id: \.self) { category in
+                                Button(category) {
+                                    selectedCategory = category
+                                }
+                            }
+                            Button("Cancel", role: .cancel) { }
+                        }
+                    }
+                    List {
+                        ForEach(sortedRudiments) { rudiment in
+                            NavigationLink(destination: RudimentPractice(rudiment: binding(for: rudiment))) {
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text(rudiment.name)
+                                            .foregroundColor(.white)
+                                        Text(rudiment.category)
+                                            .font(.caption)
+                                            .foregroundColor(.gray)
+                                    }
+                                    Spacer()
+                                    Text(String(rudiment.bpm))
+                                        .foregroundColor(.white)
+                                }
+                            }
+                            .listRowBackground(Color(red: 0.11, green: 0.11, blue: 0.12))
+                        }
+                    }
+                    .scrollContentBackground(.hidden)
+                }
+            }
+            .onAppear {
+                loadRudiments()
+            }
+        }
+    }
+    
+    private func binding(for rudiment: Rudiment) -> Binding<Rudiment> {
+        guard let index = rudiments.firstIndex(where: { $0.id == rudiment.id }) else {
+            fatalError("Rudiment not found")
+        }
+        return $rudiments[index]
+    }
+    
     func loadRudiments() {
         guard let url = Bundle.main.url(forResource: "rudiments", withExtension: "json"),
               let data = try? Data(contentsOf: url),
@@ -278,6 +331,99 @@ struct RudimentList: View {
             return
         }
         rudiments = decoded
+    }
+}
+
+struct RudimentPractice: View {
+    @Binding var rudiment: Rudiment
+    @State var isPlaying: Bool = false
+    @State private var timer: Timer?
+    @State private var clickPlayer: AVAudioPlayer?
+    @State private var accentPlayer: AVAudioPlayer?
+    @State private var lowerPlayer: AVAudioPlayer?
+    @State private var currentBeat: Int = 1
+    @State private var tempo: Double = 60
+    var body: some View {
+        ZStack {
+            Color(red:0.11, green:0.11, blue:0.12)
+            VStack {
+                Text("\(Int(tempo))")
+                    .foregroundStyle(.white)
+                    .font(.system(size: 50))
+                Slider(value: $tempo, in: 30...300)
+                    .onChange(of: tempo) {
+                        if isPlaying {
+                            stopMetronome()
+                            isPlaying = false
+                        }
+                    }
+                    .padding(.horizontal)
+                Button {
+                    isPlaying.toggle()
+                    if isPlaying {
+                        startMetronome()
+                    } else {
+                        stopMetronome()
+                    }
+                } label: {
+                    Image(systemName: isPlaying ? "pause.fill" : "play.fill")
+                        .font(.system(size: 50))
+                        .foregroundColor(.blue)
+                }
+                .frame(width: 60, height:60)
+            }
+        }
+        .onAppear {
+            tempo = Double(rudiment.bpm)
+            setupAudio()
+        }
+    }
+    func startMetronome() {
+        timer = Timer.scheduledTimer(withTimeInterval: beatInterval, repeats: true) {_ in playClick()}
+        playClick()
+    }
+    func stopMetronome() {
+        currentBeat = 1
+        timer?.invalidate()
+        timer = nil
+    }
+    func playClick() {
+        if currentBeat == 1 {
+            accentPlayer?.play()
+        } else {
+            clickPlayer?.play()
+        }
+        if currentBeat > 3 {
+            currentBeat = 1
+        } else {
+            currentBeat += 1
+        }
+    }
+    var beatInterval: TimeInterval {
+        60.0/(tempo)
+    }
+    func setupAudio() {
+        guard let url = Bundle.main.url(forResource: "tone-800", withExtension: "wav") else { return }
+        do {
+            clickPlayer = try AVAudioPlayer(contentsOf: url)
+            clickPlayer?.prepareToPlay()
+        } catch {
+            print("Error loading sound: \(error)")
+        }
+        guard let accenturl = Bundle.main.url(forResource: "tone-1000", withExtension: "wav") else { return }
+        do {
+            accentPlayer = try AVAudioPlayer(contentsOf: accenturl)
+            accentPlayer?.prepareToPlay()
+        } catch {
+            print("Error loading sound: \(error)")
+        }
+        guard let lowerurl = Bundle.main.url(forResource: "tone-800lower", withExtension: "wav") else { return }
+        do {
+            lowerPlayer = try AVAudioPlayer(contentsOf: lowerurl)
+            lowerPlayer?.prepareToPlay()
+        } catch {
+            print("Error loading sound: \(error)")
+        }
     }
 }
 
